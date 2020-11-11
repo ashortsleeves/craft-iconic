ca64b46d144c780cd6a3c24f8cc427c3ff662d9d
ashortsleeves <ashortsleeves@gmail.com>
2020-11-11 11:27:05 -0500

    search styling

--- wp-content/themes/craft-iconic/resources/views/partials/content-search.blade.php
+++ wp-content/themes/craft-iconic/resources/views/partials/content-search.blade.php
@@ -1,9 +1,20 @@
-<article @php post_class() @endphp>
+<article @php post_class('cardstyle') @endphp>
+  @if(has_post_thumbnail())
+    <div class="img-wrap jumbo-bg" style="background-image: url({{ the_post_thumbnail_url() }});">
+    </div>
+  @endif
   <header>
     <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
-    @if (get_post_type() === 'post')
-      @include('partials/entry-meta')
-    @endif
+    <h3 class="search-type">
+      @if(get_post_type() === 'post')
+        Article
+      @elseif(get_post_type() === 'project')
+        Portfolio
+      @else
+        {!!get_post_type()!!}
+      @endif
+
+    </h3>
   </header>
   <div class="entry-summary">
     @php the_excerpt() @endphp
--- wp-content/themes/craft-iconic/resources/views/search.blade.php
+++ wp-content/themes/craft-iconic/resources/views/search.blade.php
@@ -4,15 +4,37 @@
   @include('partials.page-header')
 
   @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
+    <div class="container container-sm container-404">
+      <h1><i class="fas fa-telescope"></i> {{ __('Sorry, no results were found.', 'sage') }}</h1>
+      {!! get_search_form(false) !!}
+      <button class="btn btn-lg"  onclick="goBack()"><i class="fas fa-arrow-left"></i> Go Back</button>
+
+      <script>
+      function goBack() {
+        window.history.back();
+      }
+      </script>
+
     </div>
-    {!! get_search_form(false) !!}
+  @else
+    <div class="container sm-container posts-container">
+      <h1><i class="fas fa-telescope"></i> Search Results</h1>
+
+      <div class="row">
+        <div class="col-md-8">
+          @while (have_posts()) @php the_post() @endphp
+            @include('partials.content-search')
+          @endwhile
+        </div>
+        <div class="col-md-4">
+          <div class="cardstyle">
+              @php dynamic_sidebar('sidebar-primary') @endphp
+          </div>
+        </div>
+      </div>
+    </div>
+    {!! get_the_posts_navigation() !!}
   @endif
 
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.content-search')
-  @endwhile
 
-  {!! get_the_posts_navigation() !!}
 @endsection
